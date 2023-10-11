5c1d29f0c2a8090ca4780325d75725ac9895af84
Alumno <Alumno@LAB1504-15>
2023-10-10 22:02:15 -0500

    Agregar archivos de proyecto.

---
+++ Business/BProdcut.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Data;
+
+namespace Business
+{
+    public class BProdcut
+    {
+        public List<Product>  GetByName(string Name)
+        {
+            List<Product> result = new List<Product>();
+
+            DProduct data = new DProduct();
+
+            //Obtengo todos los productos
+            var products = data.Get();
+
+            //Filtro por nombre
+            foreach (var product in products)
+            {
+                if (product.Name == Name)
+                {
+                    result.Add(product);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Data/DProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Entity;
+
+namespace Data
+{
+    public class DProduct
+    {
+        private string connectionString = "Data Source=LAB1504-15\\SQLEXPRESS;Initial Catalog=facturadb;User ID=diego;Password=hola123";
+
+        public List<Product> Get()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "ListarProductos";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                ProductId = Convert.ToInt32(reader["product_id"]),
+                                Name = reader["name"].ToString(),
+                                Price = Convert.ToDecimal(reader["price"]),
+                                Stock = Convert.ToInt32(reader["stock"]),
+                                Active = Convert.ToBoolean(reader["active"])
+                            };
+                            products.Add(product);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+
+            return products;
+        }
+    }
+}
